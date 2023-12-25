ce8f890e21b3e259fe24cea45e37d905c1faa74f
g-tejas <ttejasgarrepally@gmail.com>
2023-12-25 16:35:58 +0800

    run some basic test

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-import collections
-import hashlib
-import zlib
+import fsync
 
-BLOCK_SIZE = 4096
-
-def md5_chunk(chunk):
-    m = hashlib.md5()
-    m.update(chunk)
-    return m.hexdigest()
-
-def adler32_chunk(chunk):
-    return zlib.adler32(chunk)
-
-class RsyncLUT:
-    def __init__(self):
-        pass
-    def __getitem__(self, block_data):
-        pass
-
-
-class Chunks:
-    """
-    Data structure that holds rolling checksum for source file
-    """
-    def __init__(self):
-        self.chunks = []
+if __name__ == "__main__":
+    fsync.patch("example/src.txt", "example/dest.txt")
